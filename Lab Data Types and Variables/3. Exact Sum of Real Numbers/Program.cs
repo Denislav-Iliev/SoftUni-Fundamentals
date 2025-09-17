f12f677b03abc2e66c22d4ecf208d70cc97d9cde
Denislav-Iliev <bdhfallenangel99@gmail.com>
2025-09-17 21:44:14 +0300

    Added more solutions

---
+++ Lab Data Types and Variables/3. Exact Sum of Real Numbers/Program.cs
@@ -0,0 +1,9 @@
+﻿int count = int.Parse(Console.ReadLine());
+decimal sum = 0;
+
+for (int i = 1; i <= count; i++)
+{
+    decimal number = decimal.Parse(Console.ReadLine());
+    sum += number;
+}
+Console.WriteLine(sum);
---
+++ Lab Data Types and Variables/5. Special Numbers/Program.cs
@@ -0,0 +1,22 @@
+﻿int number = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= number; i++)
+{
+    int sum = 0;
+    int digit = i;
+
+    while (digit > 0)
+    {
+        sum += digit % 10;
+        digit /= 10;
+    }
+    bool isSpecialNumber = sum == 5 || sum == 7 || sum == 11;
+    if (isSpecialNumber)
+    {
+        Console.WriteLine($"{i} -> True");
+    }
+    else
+    {
+        Console.WriteLine($"{i} -> False");
+    }
+}
