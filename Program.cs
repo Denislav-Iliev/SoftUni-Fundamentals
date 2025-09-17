cbea049629e3722fa917984b53cd09d65fe55e1a
Denislav-Iliev <bdhfallenangel99@gmail.com>
2025-09-17 18:11:17 +0300

    Random Problem

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿int countOrders = int.Parse(Console.ReadLine());
+double orderPrice = 0;
+double totalprice = 0;
+
+for (int i = 1; i <= countOrders; i++)
+{
+    double priceperCapsule = double.Parse(Console.ReadLine());
+    int days = int.Parse(Console.ReadLine());
+    int capsuleCount = int.Parse(Console.ReadLine());
+    orderPrice += ((days * capsuleCount) * priceperCapsule);
+    Console.WriteLine($"The price for the coffee is: {orderPrice:F2}");
+    totalprice += orderPrice;
+}
+Console.WriteLine($"Total: ${totalprice:F2}");
+
